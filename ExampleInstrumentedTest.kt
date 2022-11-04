022ddb0a2531e6c56fb834bd0f968ebd820d84f8
JeninaAngelin <94626668+JeninaAngelin@users.noreply.github.com>
2022-11-04 22:10:17 +0530

    Add files via upload

---
+++ ExampleInstrumentedTest.kt
@@ -0,0 +1,9 @@
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.example.paintapp", appContext.packageName)
+    }
+}
